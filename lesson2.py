6b64cb91a87129150cd7708cb9818ed891aca0b2
dhevansh2026 <s.reddy@live.in>
2026-04-10 15:20:04 +0100

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,10 @@
+name="Dhevansh"
+age=10
+Date_of_birth="16/11/2015"
+hobby="playing football"
+
+print("My name is {0} and I am {1} years old.".format(name,age), end = " ")
+print("My Date of birth is {0} and my hobby is {1}".format(Date_of_birth,hobby), end = " ")
+
+import keyword
+print(keyword.kwlist)
