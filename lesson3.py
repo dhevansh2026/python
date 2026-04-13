e7580973bb99eda01db0f6adb06cbbaf750c87a5
dhevansh2026 <s.reddy@live.in>
2026-04-13 17:28:39 +0100

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,16 @@
+Name = "Dhevansh"
+Age = 10
+Is_student = True
+Height = 139.75
+
+print (f"Name: {Name}\nAge: {Age}\nStudent?: {Is_student}\nHeight: {Height}\n")
+print(type(Name))
+print(type(Age))
+print(type(Is_student))
+print(type(Height))
+print(int(Height))
+print(float(Age))
+
+Text = input("Enter a string: ")
+Rev_Text = Text[::-1]
+print(f"Text: {Text}\nReversed Text: {Rev_Text}")
