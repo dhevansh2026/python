962f3f35772b8da4fc530ce35419b68e211b2868
dhevansh2026 <s.reddy@live.in>
2026-04-19 12:58:25 +0100

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+Nathan="01/01/2015"
+Edward="02/02/2015"
+Prajeet="04/03/2016"
+Vihaan="01/04/2015"
+Reyansh="05/05/2016"
+
+print("Nathan's birthday is ",Nathan)
+print("Edward's birthday is ",Edward)
+print("prajeet's birthday is ", Prajeet)
+print("Vihaan's birthday is ",Vihaan)
+print("Reyansh's birthday is ",Reyansh)
