eda024638f28d92464a97c9b1f44df86b05e3cca
dhevansh2026 <s.reddy@live.in>
2026-04-24 08:08:08 +0100

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+Jacket=int(input("Enter a temurature "))
+
+if Jacket==50: 
+    print("it is the right temp for rohan to wear his jacket")
+
+if Jacket<50: 
+    print("it is too cold for rohan to wear his jacket")
+
+if Jacket>50:
+    print(" it is too hot for rohan to wear his jacket")
